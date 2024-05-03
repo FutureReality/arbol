f9bac2baca00fb06f2c53c3f13a9c8bf3fcd3a9a
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-05-03 08:50:36 +0200

    Create arbol.java

---
+++ arbol.java
@@ -0,0 +1,13 @@
+package EstructurasNoLineales;
+
+public class NodoArbol {
+	private int valor;
+	private NodoArbol nodoI;
+	private NodoArbol nodoD;
+	public NodoArbol(int valor){
+		this.valor = valor;
+		this.nodoI = null;
+		this.nodoD = null;
+	}
+
+}
